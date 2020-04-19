b9f73618aa5c858f949d7860f6890ba2c42d8735
majidgabrlo <61796004+majidgabrlo@users.noreply.github.com>
2020-04-20 04:04:39 +0430

    Adding the files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React ,{Component} from 'react';
 import './App.css';
+import { CardList } from './components/card-list/card-list-component';
+import { SearchBox } from './components/search-box/search-box-component'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  
+  constructor(){
+    super();
+    this.state={
+      monsters:[],
+      searchBox:''
+    }
+  }
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res=>res.json())
+    .then(users=>this.setState({ monsters:users }))
+  }
+  changeHandler=(e)=>{
+    this.setState({searchBox : e.target.value})
+  }
+  render(){
+    const { monsters, searchBox } =this.state
+    const FilteredMonsters=monsters.filter(monster=>
+      monster.name.toLowerCase().includes(searchBox.toLowerCase()))
+    return(
+      <div className="App">
+        <h1>MONSTERS ROLODEX</h1>
+        {/* setState is asynchronous and the second parameter execute after the setState */}
+        <SearchBox placeholder='Search monsters'
+          handlechange={this.changeHandler} />      
+        <CardList monsters={FilteredMonsters} />
+      </div>
+    )
+  }
 }
 
 export default App;
