bea38cc26085dff14638c087c12828e887784cf1
majidgabrlo <61796004+majidgabrlo@users.noreply.github.com>
2020-04-20 04:03:39 +0430

    Updates

---
+++ precache-manifest.d8fa428e69c559fe68d777562c5bb6f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ed15e48dbcd2a490eaea06d8a7706ef",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "880b91342666d2986d63",
+    "url": "/monsters/static/css/main.aaa56d15.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "880b91342666d2986d63",
+    "url": "/monsters/static/js/main.a3994501.chunk.js"
+  },
+  {
+    "revision": "e20b59329f3960f98de4",
+    "url": "/monsters/static/js/runtime-main.e2917106.js"
+  }
+]);
